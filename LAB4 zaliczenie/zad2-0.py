f96bd1e78018dfb73007ed81d09b741167385575
blazej35 <blazej.klepacki1@gmail.com>
2023-11-08 10:18:40 +0100

    Solved excercise 2-0

---
+++ LAB4 zaliczenie/zad2-0.py
@@ -0,0 +1,13 @@
+# Napisz funkcję, która liczby mniejsze od 0 podmienia na 0, a inne przepisuje (appenduje do nowej tabeli)
+
+def negative_to_zero(list):
+    new_list = []
+    for n in list:
+        if n < 0:
+            new_list.append(0)
+        else:
+            new_list.append(n)
+    return new_list
+
+
+print(negative_to_zero([-2, 0, 2]))
